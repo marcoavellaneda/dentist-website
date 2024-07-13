d9a778c1276ddd86c61691308255b8009792bc6a
Marco Avellaneda <marco.avellanedaluz@gmail.com>
2024-07-13 19:32:55 -0300

    home screen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,19 +3,20 @@ import HeroBtns from './HeroBtns';
 
 function Hero() {
   return (
-    <div className="relative w-full h-screen overflow-clip ">
+    <div className="relative w-full h-[96%] z-0 pb-1 min-[400px]:pb-24">
       <img
         src={heroBackground}
         alt="Background"
-        className="absolute left-0 w-full object-cover object-center"
+        className="absolute object-cover object-center w-full max-h-full "
       />
-      <div className="relative pt-96 text-white top-28">
-        <h1 className="text-4xl pl-16 sm:text-10xl md:text-5xl lg:text-6xl font-bold leading-tight">
+      <div className="relative pt-72 text-white top-28">
+        <h1 className="text-4xl pl-16 sm:text-10xl md:text-5xl lg:text-6xl font-bold leading-tight min-[400px]:pt-44 ">
           +500 <br /> Atendimentos <br />realizados
         </h1>
       </div>
       <div className='relative'>
         <HeroBtns/>
+        <img className='absolute z-20 w-0 min-[400px]:w-10 ml-52 mt-4' src="src/assets/mobile-scroll.png " alt="" />
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,27 @@
 import homeBackground from '../assets/mobile-background-home.png';
 import Hero from "../components/Hero";
+import Reviews from '../components/Reviews';
 
 function Home () {
   return (
     <div>
       <h1 className="absolute z-10">test</h1>
       <Hero/>
-      <div className="relative">
+
+
+      <div className="absolute">
         <img
         src={homeBackground}
         alt="Background"
         className=""
       />
       </div>
+
+
+      <div className='absolute z-10 w-full'>
+        <Reviews/>
+      </div>
+        
     </div>
   );
 }
