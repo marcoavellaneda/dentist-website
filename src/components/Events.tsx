b1c090f991b1909b0e8434c71c6de94a4b137b3b
Marco Avellaneda <marco.avellanedaluz@gmail.com>
2024-07-20 11:31:57 -0300

    Fix responsividade layer csv

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -3,7 +3,7 @@ import EventList from "./EventList";
 function Events() {
   return (
     <div className="pt-4">
-      <img className="absolute  mt-12 right-1 h-[400px] w-[300px] " src="src/assets/layer-bg.png" alt="" />
+      <img className="absolute  mt-12 right-1 h-[45%] w-[80%] " src="src/assets/layer-bg.png" alt="" />
       <img className="absolute  ml-4 w-[30%]" src="src/assets/event-icon.png" alt="" />
 
       <div className="mt-20 w-[10%] relative left-[35%] text-white font-bold">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import ProjectList from "./ProjectList";
 function Projects() {
   return (
     <div className="pt-4 pb-80">
-      <img className="absolute  mt-12 right-1 h-[400px] w-[300px] " src="src/assets/layer-bg.png" alt="" />
+      <img className="absolute  mt-12 right-1 h-[45%] w-[80%]" src="src/assets/layer-bg.png" alt="" />
       <img className="absolute  ml-4 w-[30%]" src="src/assets/project-icon.png" alt="" />
 
       <div className="mt-20 relative w-[10%] left-[35%] text-white font-bold">
